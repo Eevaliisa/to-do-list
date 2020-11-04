9a3a20f7d6296ec144bc6f14a531e315540b0c78
eevaliisa <eevalr@gmail.com>
2020-11-04 23:04:30 +0200

    added functionality for the user to create a list

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
 
@@ -17,11 +21,17 @@ app.get("/", (req, res) => {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { day });
+  res.render("list", { day, newListItems: items });
 
 });
 
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
 
+  items.push(item);
+
+  res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
