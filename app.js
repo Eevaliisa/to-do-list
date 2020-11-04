86880cffe53e28b5cde4dfa57ceab9c4c3dd8f81
eevaliisa <eevalr@gmail.com>
2020-11-04 22:29:31 +0200

    Changed day of the week rendering in app.get method, added list element to UI.

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
 
   let today = new Date();
-  let currentDay = today.getDay();
-  let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let day = week[currentDay];
+
+  let options = {
+    weekday: "long",
+    day:"numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { day });
 
